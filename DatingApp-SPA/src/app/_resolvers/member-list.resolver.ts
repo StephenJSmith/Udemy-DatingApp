f1fede715e18789fc442b1e4c6455dae27856e6d
Stephen Smith <stephensmith_aus@yahoo.com.au>
2019-08-16 07:50:20 +1000

    End of Section 14

--- DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
+++ DatingApp-SPA/src/app/_resolvers/member-list.resolver.ts
@@ -10,6 +10,9 @@ import { UserService } from '../_services/user.service';
 
 @Injectable()
 export class MemberListResolver implements Resolve<User[]> {
+  pageNumber = 1;
+  pageSize = 5;
+
   constructor(
     private userService: UserService,
     private router: Router,
@@ -17,7 +20,7 @@ export class MemberListResolver implements Resolve<User[]> {
   ) { }
 
   resolve(route: ActivatedRouteSnapshot): Observable<User[]> {
-    return this.userService.getUsers()
+    return this.userService.getUsers(this.pageNumber, this.pageSize)
       .pipe(
         catchError(error => {
           this.alertify.error('Problem retrieving data');
--- DatingApp-SPA/src/app/members/member-list/member-list.module.ts
+++ DatingApp-SPA/src/app/members/member-list/member-list.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MemberListComponent } from './member-list.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ],
   declarations: [MemberListComponent]
 })
